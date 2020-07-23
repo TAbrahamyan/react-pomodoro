4aa2857523388f8d4a3f3d4c2ebae1f7408f7f90
Tigran Abrahamyan <tabrahamyan@Tigrans-MacBook-Pro.local>
2020-07-23 09:32:27 +0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
+import './scss/components/_app.scss';
+
+const Button: any = styled.button`
+  background: ${(props: any) => props.primary ? "palevioletred" : "white"};
+  color: ${(props: any) => props.primary ? "white" : "palevioletred"};
+  border: 2px solid palevioletred;
+  padding: 0.3rem 1.3rem;
+  font-size: 1.5em;
+  outline: none;
+  cursor: pointer;
+`;
+
+const StyledSpan: any = styled.span`
+  font-size: 1.5em;
+  font-weight: 600;
+  margin: 0;
+  padding: 0;
+`;
+
+export const App: React.FC = () => {
+  const [ counter, setCounter ] = React.useState<number>(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Button onClick={() => setCounter(counter + 1)}>+1</Button>
+      <StyledSpan>{ counter }</StyledSpan>
+      <Button onClick={() => setCounter(counter - 1)}>-1</Button>
     </div>
   );
 }
-
-export default App;
